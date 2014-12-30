900f77c2c56638d33d942a5b99b08d18203a1961
13lcp2000 <leo011093@hotmail.com>
2014-12-30 02:10:57 -0500

    Translating and Restructuring this project

---
+++ src/parcialDiego/Student.java
@@ -0,0 +1,46 @@
+package parcialDiego;
+
+public class Student {
+
+    private double nota1 = 0.0;
+    private double nota2 = 0.0;
+    private double nota3 = 0.0;
+    private double definitiva = 0.0;
+
+    public Student() {
+    }
+
+    public Student(double nota1, double nota2, double nota3) {
+
+        this.nota1 = nota1;
+        this.nota2 = nota2;
+        this.nota3 = nota3;
+        this.definitiva = toCalculateFinalRating();
+    }
+
+    public void setNota1(double nota1) {
+        this.nota1 = nota1;
+    }
+
+    public void setNota2(double nota2) {
+        this.nota2 = nota2;
+    }
+
+    public void setNota3(double nota3) {
+        this.nota3 = nota3;
+    }
+
+    public double toCalculateFinalRating() {
+        return ((nota1 + nota2 + nota3) / 3);
+    }
+
+    @Override
+    public String toString() {
+        return "Las notas del estudiante son: \n"
+                + "nota1=" + nota1
+                + ",\n nota2=" + nota2
+                + ",\n nota3=" + nota3
+                + ",\n definitiva=" + definitiva;
+    }
+
+}
